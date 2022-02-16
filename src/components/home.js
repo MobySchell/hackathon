f5c98407861abadc2255cf924cb32a50090321df
Joshua Schell <jagschell2000@gmail.com>
2022-02-16 22:12:32 +0200

    Test

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React Test</h1>
+      </div>
+    );
+  }
+}
