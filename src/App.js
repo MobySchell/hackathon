38c47d7d2638aa00328e893abab23c58804a6bff
Joshua Schell <jagschell2000@gmail.com>
2022-02-20 20:40:05 +0200

    Added Firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import "./App.css";
+import React, { Component } from "react";
 
 import { Routes, Route } from "react-router-dom";
+import firebase from "./firebase/firebase";
 
 import Home from "./components/Home.js";
 import About from "./components/About.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.db = firebase.firestore();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAeuMa0Kyjglrcij7PlCBk2RGCHn0fhIqI",
+  authDomain: "hackathon-89908.firebaseapp.com",
+  projectId: "hackathon-89908",
+  storageBucket: "hackathon-89908.appspot.com",
+  messagingSenderId: "142082480197",
+  appId: "1:142082480197:web:d4de1046171d7b28366b2c",
+  measurementId: "G-0RET17MK3F",
+});
+
+export default firebase;
