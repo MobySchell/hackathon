0c7ecc25f0d69cd7791e3b99c67e5e02d8343884
Joshua Schell <jagschell2000@gmail.com>
2022-03-06 21:19:23 +0200

    Added additional links to pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
-import "../../node_modules/bootstrap-icons/icons/yin-yang.svg";
+import "../../node_modules/bootstrap-icons/icons/sunset.svg";
+import "../../node_modules/bootstrap-icons/icons/list.svg";
 import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
     return (
       <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg border-bottom border-primary border-3 shadow-sm">
           <div className="container-fluid container">
             <Link className="navbar-brand  mx-5" to="/">
               <i
-                className="bi bi-yin-yang text-danger"
+                className="bi bi-sunset text-primary"
                 style={{ fontSize: 35 }}
               ></i>
             </Link>
@@ -24,21 +25,59 @@ export default class Navbar extends Component {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span className="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon d-flex justify-content-center align-items-center">
+                <i className="bi bi-list" style={{ fontSize: 35 }}></i>
+              </span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
+              <ul className="navbar-nav navbar-right">
                 <li className="nav-item">
-                  <Link className="nav-link active" to="/">
-                    <button type="button" className="btn btn-outline-primary">
-                      Home
+                  <Link className="nav-link" to="/admin">
+                    <button
+                      type="button"
+                      className="btn btn-outline-primary shadow-sm"
+                    >
+                      Admin
                     </button>
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link active" to="/about">
-                    <button type="button" className="btn btn-outline-primary">
-                      About
+                  <Link className="nav-link" to="/">
+                    <button
+                      type="button"
+                      className="btn btn-outline-primary shadow-sm"
+                    >
+                      Login
+                    </button>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">
+                    <button
+                      type="button"
+                      className="btn btn-outline-primary shadow-sm"
+                    >
+                      Register
+                    </button>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/job-listings">
+                    <button
+                      type="button"
+                      className="btn btn-outline-primary shadow-sm"
+                    >
+                      Job Listings
+                    </button>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/profile">
+                    <button
+                      type="button"
+                      className="btn btn-outline-primary shadow-sm"
+                    >
+                      Profile
                     </button>
                   </Link>
                 </li>
