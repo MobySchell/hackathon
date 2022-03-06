003a69d443f809130bb1954d2c2d60cb8fa1a226
Joshua Schell <jagschell2000@gmail.com>
2022-03-06 21:17:27 +0200

    Added necessary Routes for pages

--- src/App.js
+++ src/App.js
@@ -4,22 +4,29 @@ import React, { Component } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-import Home from "./components/Home.js";
-import About from "./components/About.js";
+import Login from "./components/Login.js";
+import Register from "./components/Register.js";
+import Admin from "./Admin/Admin";
+import PendingUsers from "./Admin/PendingUsers";
+import Users from "./Admin/Users";
+import Jobs from "./Admin/Jobs";
+import JobListings from "./components/JobListings";
+import Profile from "./components/Profile";
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-
-  // }
-
   render() {
     return (
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/job-listings" element={<JobListings />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/pending-users" element={<PendingUsers />} />
+          <Route path="/admin/users" element={<Users />} />
+          <Route path="/admin/jobs" element={<Jobs />} />
         </Routes>
       </div>
     );
